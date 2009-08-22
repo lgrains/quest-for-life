6f09f41492f089792e06aeb51c3c29f4442493d4
www <www@li107-31.members.linode.com>
2009-08-22 18:59:25 +0000

    Finalized production mailer settings.

--- config/initializers/smtp_gmail.rb
+++ config/initializers/smtp_gmail.rb
@@ -3,5 +3,8 @@ require "smtp_tls"
 
 mailer_config = File.open("#{RAILS_ROOT}/config/mailer.yml")
 mailer_options = YAML.load(mailer_config)
+mailer_options[:user_name] = $private_settings.mail_user
+mailer_options[:password] = $private_settings.mail_password
 
 ActionMailer::Base.smtp_settings = mailer_options
+
