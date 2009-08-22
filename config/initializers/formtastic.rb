6b9275063a4f3cd7649cf2bf7c95e1dca417d9c0
Jeremy Weiskotten <jweiskotten@patientslikeme.com>
2009-08-22 02:58:58 -0400

    Form style cleanup

---
+++ config/initializers/formtastic.rb
@@ -0,0 +1,4 @@
+Formtastic::SemanticFormBuilder.default_text_field_size = 20
+Formtastic::SemanticFormBuilder.all_fields_required_by_default = false
+# Formtastic::SemanticFormBuilder.required_string = "(required)"
+# Formtastic::SemanticFormBuilder.priority_countries = ["Australia", "New Zealand"]
