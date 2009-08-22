ea32bc57491ca7d41dfcae7bdf8edee1d02acd7d
Jeremy Weiskotten <jweiskotten@patientslikeme.com>
2009-08-21 21:48:34 -0400

    Rational options admin UI

--- app/controllers/rational_options_controller.rb
+++ app/controllers/rational_options_controller.rb
@@ -3,6 +3,11 @@ class RationalOptionsController < ApplicationController
 
   make_resourceful do
     actions :all
+    
+    response_for :create, :update, :destroy do |format|
+      format.html { redirect_to rational_options_path }
+    end
+    
   end  
   
 end
--- app/models/rational_option.rb
+++ app/models/rational_option.rb
@@ -5,14 +5,15 @@ class RationalOption < ActiveRecord::Base
   
   validates_numericality_of :numerator, :greater_than_or_equal_to => 0
   validates_numericality_of :denominator, :greater_than => 0
+  validates_uniqueness_of :quotient
   
-  before_validation_on_save :calculate_quotient
+  before_validation :calculate_quotient
   
   attr_accessible :numerator, :denominator
   
   private
   
   def calculate_quotient
-    self.quotient = self.numerator / self.denominator
+    self.quotient = self.numerator.to_f / self.denominator.to_f
   end
 end
