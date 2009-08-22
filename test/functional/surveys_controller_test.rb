3404b8a5030a2e9a6fc0d2476da73f2a13daede0
Jeremy Weiskotten <jweiskotten@patientslikeme.com>
2009-08-22 01:39:03 -0400

    test

--- test/functional/surveys_controller_test.rb
+++ test/functional/surveys_controller_test.rb
@@ -4,5 +4,11 @@ class SurveysControllerTest < ActionController::TestCase
   test "new" do
     get :new
     assert_response :ok
+    assert_not_nil assigns(:current_object)
   end
+  
+  # test "create" do
+  #   post :create, 
+  #   
+  # end
 end
