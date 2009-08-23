6ea6b377712ecec8097e5eb2218e8d7fb3651f88
Joe Lind <jlind@patientslikeme.com>
2009-08-22 21:13:33 -0400

    model changes to add a login to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,6 @@ class User < ActiveRecord::Base
   include Clearance::User
   has_many :survey_groups
   
-  attr_accessible :identity_url
+  attr_accessible :identity_url, :login
+  validates_uniqueness_of :login, :case_sensitive => false, :allow_nil => true
 end
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -1,5 +1,12 @@
-require 'test_helper'
+File.dirname(__FILE__) + '/../test_helper'
 
 class UserTest < ActiveSupport::TestCase
+  def setup
+    @user = Factory(:user)
+  end
   should_have_many :survey_groups
+  should_have_db_column :login, :type => :string
+  should_validate_uniqueness_of :login, :case_sensitive => :false
+  should_allow_mass_assignment_of :login, :identity_url
+  should_not_allow_mass_assignment_of :admin
 end
