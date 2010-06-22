4558dbe66cd9f68e64a67a3ed3f8d89c5978dcfa
Steven Hammond <shammond@northpub.com>
2010-06-22 12:26:07 -0400

    Tweak to sample charting page.

--- public/javascripts/chart-content.js
+++ public/javascripts/chart-content.js
@@ -65,69 +65,6 @@ var chart;
 $(document).ready(function() {
 	chart = new Highcharts.Chart({
 		chart: {
-			renderTo: 'ages-all',
-			defaultSeriesType: 'column'
-		},
-		title: {
-			text: 'Estimates for all ages'
-		},
-		xAxis: {
-			categories: ['1', '10', '100', '1000']
-		},
-		yAxis: {
-			title: {
-				text: 'Number of Responses'
-			}
-		},
-		tooltip: {
-			formatter: function() {
-				return '' + this.y +' users responded with '+ this.x.toLowerCase();
-			}
-		},
-		plotOptions: {
-			column: {
-				data: 'data-ages-all',
-				// define a custom data parser function for both series
-				dataParser: function(data) {
-					var table = document.getElementById(data),
-						// get the data rows from the tbody element
-						rows = table.getElementsByTagName('tbody')[0].getElementsByTagName('tr'),
-						// define the array to hold the real data
-						result = [], 
-						// decide which column to use for this series
-						column = { 'Count': 0 }[this.options.name];
-						
-					// loop through the rows and get the data depending on the series (this) name
-					for (var i = 0; i < rows.length; i++) {						
-						result.push(
-							parseInt(
-								rows[i].getElementsByTagName('td')[column]. // the data cell
-								innerHTML
-							)
-						);
-					}
-					return result;
-				}
-			}
-		},
-		credits: {
-			enabled: false
-		},
-		legend: {
-			enabled: false
-		},
-		series: [{
-			name: 'Count'
-		}]
-	});
-	
-	
-});
-
-var chart;
-$(document).ready(function() {
-	chart = new Highcharts.Chart({
-		chart: {
 			renderTo: 'gender-group',
 			defaultSeriesType: 'column'
 		},
@@ -135,7 +72,7 @@ $(document).ready(function() {
 			text: 'Estimates by males'
 		},
 		xAxis: {
-			categories: ['Male', 'Female', 'Unknown']
+			categories: ['1', '10', '100', '1000']
 		},
 		yAxis: {
 			title: {
@@ -191,14 +128,14 @@ var chart;
 $(document).ready(function() {
 	chart = new Highcharts.Chart({
 		chart: {
-			renderTo: 'gender-all',
+			renderTo: 'all',
 			defaultSeriesType: 'column'
 		},
 		title: {
-			text: 'Estimates for all genders'
+			text: 'All Estimates'
 		},
 		xAxis: {
-			categories: ['Male', 'Female', 'Unknown']
+			categories: ['1', '10', '100', '1000']
 		},
 		yAxis: {
 			title: {
@@ -212,7 +149,7 @@ $(document).ready(function() {
 		},
 		plotOptions: {
 			column: {
-				data: 'data-gender-all',
+				data: 'data-all',
 				// define a custom data parser function for both series
 				dataParser: function(data) {
 					var table = document.getElementById(data),
@@ -246,6 +183,5 @@ $(document).ready(function() {
 			name: 'Count'
 		}]
 	});
-	
 	
 });
