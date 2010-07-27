f8a2c14156c3632f0ae1990d24e3b8c7dd26c9ee
Steven Hammond <shammond@northpub.com>
2010-07-27 07:22:15 -0400

    Upgraded active_hash gem to remove test warnings.

--- config/environment.rb
+++ config/environment.rb
@@ -40,7 +40,7 @@ Rails::Initializer.run do |config|
   config.gem 'chriseppstein-compass', :version => '0.8.10', :lib => 'compass', :source => 'http://gems.github.com'
   config.gem 'jcnetdev-better_partials', :version => '>= 1.1', :lib => 'better_partials', :source => 'http://gems.github.com'
   config.gem 'binarylogic-searchlogic', :version => '2.2.3', :lib => 'searchlogic', :source => 'http://gems.github.com'
-  config.gem 'zilkey-active_hash', :version => '0.6.1', :lib => 'active_hash'
+  config.gem 'active_hash', :version => '0.8.1', :lib => 'active_hash'
   config.gem 'exceptional', :version => '2.0.7'
   
   # Skip frameworks you're not going to use. To use Rails without a database,
--- vendor/gems/zilkey-active_hash-0.6.1/lib/active_hash.rb
+++
@@ -1,5 +0,0 @@
-require 'rubygems'
-require 'activesupport'
-require 'active_hash/base'
-require 'active_file/base'
-require 'active_yaml/base'
